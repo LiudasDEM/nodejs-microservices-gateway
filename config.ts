b98545efac434b46c5018221d30169d0705f1f9e
LiudasDEM <liudas.demikis97@gmail.com>
2020-05-11 21:48:23 +0300

    use winston logstash and console logger

--- config.ts
+++ config.ts
@@ -1,3 +1,8 @@
 export default {
 	httpPort: process.env.HTTP_PORT || 7070,
+	logger: {
+		index: process.env.ELASTIC_STACK_INDEX,
+		logstashHost: process.env.ELASTIC_STACK_LOGSTASH_HOST,
+		environment: process.env.ELASTIC_STACK_ENVIRONMENT,
+	},
 }
